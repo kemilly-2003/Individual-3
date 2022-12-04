faf9b7275452f2d5a5f853e9fdd115b13aa78f82
kemilly-2003 <kemillyvitoriacc@gmail.com>
2022-12-03 22:30:09 -0300

    exercicios

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const {response} = require('express')
+const {uuid} = require('uuidv4') 
+
+
+const app = express()
+app.use(express.json())
+const alunos = []
+
+app.get('/Cinema',(request, response) =>{
+    return response.json(Cinema);
+
+})
+app.post('/Cinema', (request, response) =>{
+    const {id,poltrona, telão, filmes, escada} = request.body
+    const cinema = {poltrona, telão, filmes, escada};
+    cinema.push(cinema)
+    return response.status(201).json(cinema);
+
+})
+app.put('/cinema/cinema:id',(request, response) => {
+    const { Cinema_id } = request.params
+    const { poltrona, telão, filmes, escada } = request.body
+    const newCinema = {id, poltrona, telão, filmes, escada}
+    const cinemaindex = alunos.findIndex(aluno => aluno.id == id)
+    cinema[cinemaindex] = newCinema;
+    return response.json(newCinema)
+})
+
+app.delete('/Cinema/:cinema_id',(request, response) => {
+    const { Cinema_id } = request.params
+    const cinemaindex = cinema.findIndex(cinema => cinema.id == id)
+    cinema.splice(cinemaindex, 1)
+    return response.status(204). json('deleted');
+})
+
+app.listen(8181, () =>{
+    console.log('O servidor foi iniciadio!')
+})
